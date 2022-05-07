da245474cd0ca54411b0e35e63074715fd4c4025
Zach Lefler <leaving.valhalla@gmail.com>
2022-05-07 13:17:37 -0700

    commit tree b6b20941e6807f068d6f8ca4e0cb469e6394998e

--- lib/my_set.rb
+++ lib/my_set.rb
@@ -1,1 +1,42 @@
-# your code here
+class MySet
+
+    def initialize(enumerable = [])
+        @hash = {}
+        enumerable.each do |value|
+            @hash[value] = true
+        end
+    end
+
+    def include?(value)
+        @hash.has_key?(value)
+    end
+
+    def add(value)
+        @hash[value] = true
+        self
+    end
+
+    def delete(value)
+        @hash.delete(value)
+        self
+    end
+        
+    def size
+        @hash.size
+    end
+
+    def clear
+        @hash = {}
+        self
+    end
+
+    def each
+        @hash.each
+        self
+    end
+
+    def inspect
+        return "MySet: #{@hash.keys}"
+    end
+
+end
